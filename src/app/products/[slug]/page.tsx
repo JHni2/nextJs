76b747859e60fbed067b3006a54f7f72730de84e
Jihyun <wlgus0260@naver.com>
2023-05-23 11:34:10 +0900

    페이지 미리 생성

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -7,3 +7,11 @@ type Props = {
 export default function pants({ params }: Props) {
   return <h1>{params.slug} 페이지!</h1>;
 }
+
+// 미리 페이지를 만들 고 싶을 때 경로 지정
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+  return products.map((product) => ({
+    slug: product,
+  }));
+}
