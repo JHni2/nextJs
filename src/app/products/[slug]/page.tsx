e99aa6024a4c9a664c60eb2db7cf4df5a3e8f87e
Jihyun <wlgus0260@naver.com>
2023-05-23 17:09:33 +0900

    SSG 렌더링

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import { getProduct, getProducts } from '@/service/products';
+import { notFound } from 'next/navigation';
+
 type Props = {
   params: {
     slug: string;
@@ -10,14 +13,20 @@ export function generateMetadata({ params }: Props) {
   };
 }
 
-export default function pants({ params }: Props) {
-  return <h1>{params.slug} 페이지!</h1>;
+export default async function Product({ params: { slug } }: Props) {
+  const product = await getProduct(slug);
+
+  if (!product) {
+    notFound();
+  }
+
+  return <h1>{product.name} 페이지!</h1>;
 }
 
 // 미리 페이지를 만들 고 싶을 때 경로 지정
-export function generateStaticParams() {
-  const products = ['pants', 'skirt'];
-  return products.map((product) => ({
-    slug: product,
+export async function generateStaticParams() {
+  const products = getProducts();
+  return (await products).map((product) => ({
+    slug: product.id,
   }));
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,16 @@
+import { getProducts } from '@/service/products';
 import Link from 'next/link';
 
-const products = ['shirt', 'pants', 'skirt', 'shoes'];
+export default async function index() {
+  const products = getProducts();
 
-export default function index() {
   return (
     <>
       <h1>제품 소개 페이지!</h1>
       <ul>
-        {products.map((product, index) => (
+        {(await products).map((product, index) => (
           <li key={index}>
-            <Link href={`/products/${product}`}>{product}</Link>
+            <Link href={`/products/${product.id}`}>{product.name}</Link>
           </li>
         ))}
       </ul>
