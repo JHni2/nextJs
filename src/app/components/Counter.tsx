5c5897854bcf922993b7cd3837f139476279a117
Jihyun <wlgus0260@naver.com>
2023-05-23 16:11:01 +0900

    Clinet 컴포넌트

---
+++ src/app/components/Counter.tsx
@@ -0,0 +1,15 @@
+// client 컴포넌트라고 명시
+'use client';
+
+import { useState } from 'react';
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>{count}</p>
+      <button onClick={() => setCount((num) => num + 1)}>숫자 증가 시키기</button>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import { Inter } from '@next/font/google';
+import Counter from './components/Counter';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <h1>홈페이지</h1>;
+  return (
+    <>
+      <h1>홈페이지</h1>
+      <Counter />
+    </>
+  );
 }
