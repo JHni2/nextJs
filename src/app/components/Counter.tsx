f631caf5841eaf0edc03a1f88b3519f5179a8186
Jihyun <wlgus0260@naver.com>
2023-05-23 16:29:49 +0900

    동작 원리 분석 (브라우저 상에서 클릭 이벤트 처리하기)

--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+  console.log('클라이언트');
 
   return (
     <>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Counter from './components/Counter';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  console.log('서버');
+
   return (
     <>
       <h1>홈페이지</h1>
