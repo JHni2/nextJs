83e88190af0946315329a1b5d0c2b74fc427d074
Jihyun <wlgus0260@naver.com>
2023-05-24 15:36:06 +0900

    Redirect

--- next.config.js
+++ next.config.js
@@ -11,6 +11,20 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/products/deleted_forever', // 이 경로로 오면
+        destination: '/products', // 이 경로로 redirect
+        permanent: true, // 영원히!
+      },
+      {
+        source: '/products/deleted_temp', // 이 경로로 오면
+        destination: '/products', // 이 경로로 redirect
+        permanent: false, // 일시적!
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
