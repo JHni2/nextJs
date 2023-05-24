ba852c16c336a75490ad2f5de0fbb673faf1ac44
Jihyun <wlgus0260@naver.com>
2023-05-24 14:40:17 +0900

    이미지

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,16 +1,19 @@
 import { getProducts } from '@/service/products';
 import Link from 'next/link';
 import MeowArticle from '../components/MeowArticle';
+import clothesImage from 'public/images/clothesImage.jpg';
+import Image from 'next/image';
 
 export const revalidate = 3; // 3초
 
 export default async function Products() {
-  throw new Error(); // 에러 UI 확인용
   const products = getProducts();
 
   return (
     <>
       <h1>제품 소개 페이지!</h1>
+      <Image src={clothesImage} alt="localClothes" />
+      <Image src="https://images.unsplash.com/photo-1441986300917-64674bd600d8" alt="lothes" width={400} height={400} />
       <ul>
         {(await products).map((product, index) => (
           <li key={index}>
